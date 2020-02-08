f54c46fc5a409f6273f646d0ad629e7a691d3ae0
Alenvoan <novannanega@gmail.com>
2020-02-08 22:07:19 +0700

    ppdb public revisi

--- app/admin/modules/ppdb/views/dashboard/dashboard.php
+++ app/admin/modules/ppdb/views/dashboard/dashboard.php
@@ -1,73 +1,71 @@
 <div class="app-main__outer">
     <div class="app-main__inner">
-        <div class="row">
+        <div class="row" style=" display: block;
+  margin-left: auto;
+  margin-right: auto;">
             <div class="col-lg-12">
                 <div class="main-card mb-3 card">
                     <div class="card-body" style="padding: 50px">
                         <div class="row">
-                            <div class="col-sm-6">
-                                <center>
-                                    <img src="<?php echo base_url(); ?>assets/logo.png" style="max-height: 230px">
-                                    <div style="margin-top: 5px"></div>
-                                    <h3>Selamat Datang</h3>
-                                    <h3>Penerimaan Peserta Didik Baru</h3>
-                                    <h3><b>SMP NEGERI 1 ALAS BARAT</b></h3>
-                                </center>
-                            </div>
-                            <div class="col-md-6 ">
-                                <div class="card widget-content">
-                                    <div class="widget-content-outer">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left">
-                                                <div class="widget-heading">Jumlah Pendaftar</div>
-                                                <div class="widget-subheading">Saat Ini</div>
-                                            </div>
-                                            <div class="widget-content-right">
-                                                <div class="widget-numbers text-success">1896</div>
-                                            </div>
-                                        </div>
+                            <div class="col-sm-12" style="text-align: center">
+                                <img src="<?php echo base_url(); ?>assets/logo.png" style="max-height: 230px">
+                                <div style="margin-top: 5px"></div>
+                                <h3>Selamat Datang</h3>
+                                <h3>Calon Peserta Didik Baru</h3>
+                                <h3><b>SMP NEGERI 1 ALAS BARAT</b></h3>
+                                <br>
+                                <div>
+                                    <div class="col-md-6 " style=" display: block;
+  margin-left: auto;
+  margin-right: auto;">
+                                        <a href="<?php echo base_url() . index_page(); ?>ppdb/pendaftaran"><button class="btn btn-primary btn-lg">Pendaftaran</button></a>
+                                        <a href="<?php echo base_url() . index_page(); ?>ppdb/pengumuman"><button class="btn btn-primary btn-lg">Pengumuman</button></a>
                                     </div>
                                 </div>
                                 <br>
-                                <div class="card widget-content">
-                                    <div class="widget-content-outer">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left">
-                                                <div class="widget-heading">Biodata Sekolah</div>
-                                            </div>
-                                        </div>
+                                <div class="col-md-6 " style=" display: block;
+  margin-left: auto;
+  margin-right: auto;">
+                                    <div class="card widget-content">
+                                        <center>
+                                            <div class="widget-content-outer">
+                                                <div class="">
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Biodata Sekolah</div>
+                                                    </div>
+                                                </div>
 
-                                        <div style="margin-top: 2px;">
-                                            <hr>
-                                            <div class="row" style="padding: 10px">
-                                                <div class="col-sm-3">
-                                                    Alamat
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas dolores sunt illum harum doloremque pariatur ipsam delectus? Aperiam, ducimus eaque blanditiis atque hic dicta quisquam voluptate velit debitis earum?
+                                                <div style="margin-top: 2px;">
+                                                    <hr>
+                                                    <div class="row" style="padding: 10px">
+                                                        <div class="col-sm-3">
+                                                            Alamat
+                                                        </div>
+                                                        <div class="col-sm-9">
+                                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas dolores sunt illum harum doloremque pariatur ipsam delectus? Aperiam, ducimus eaque blanditiis atque hic dicta quisquam voluptate velit debitis earum?
+                                                        </div>
+                                                    </div>
+                                                    <div class="row" style="padding: 10px">
+                                                        <div class="col-sm-3">
+                                                            Email
+                                                        </div>
+                                                        <div class="col-sm-9">
+                                                            coba @ gmail.com
+                                                        </div>
+                                                    </div>
+                                                    <div class="row" style="padding: 10px">
+                                                        <div class="col-sm-3">
+                                                            No Telp
+                                                        </div>
+                                                        <div class="col-sm-9">
+                                                            081351262138712
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="row" style="padding: 10px">
-                                                <div class="col-sm-3">
-                                                    Email
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    coba @ gmail.com
-                                                </div>
-                                            </div>
-                                            <div class="row" style="padding: 10px">
-                                                <div class="col-sm-3">
-                                                    No Telp
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    081351262138712
-                                                </div>
-                                            </div>
-                                        </div>
+                                        </center>
                                     </div>
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
